f175397c11d52cedd7913dc1f52f06a81c16e2ce
Artem Shymchuk <artem.shymchuk@eleks.com>
2020-04-29 08:44:56 +0300

    add db data seed

---
+++ seed.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const MongoClient = require('mongodb').MongoClient;
+
+(async () => {
+  const client = await MongoClient.connect('mongodb://localhost:27017');
+  const db = client.db('test-a');  
+
+  await db.collection('products').insertMany([
+    {
+      id: 1,
+      name: 'Apples',
+      price: 1
+    },
+    {
+      id: 2,
+      name: 'Soup',
+      price: 0.65
+    },
+    {
+      id: 3,
+      name: 'Bread',
+      price: 0.8
+    },
+    {
+      id: 4,
+      name: 'Milk',
+      price: 1.3
+    },
+  ]);
+
+  await db.collection('offers').insertMany([
+    {
+      type: 'discount',
+      details: {
+          productId: 1,
+          discountSize: 0.1
+      },
+      startDate: '',
+      endDate: ''
+    },
+    {
+      type: 'package',
+      details: [
+          {
+              productId: 2,
+              requiredAmount: 2,
+          },
+          {
+              productId: 3,
+              discountSize: 0.5,
+          },
+      ],
+      startDate: '',
+      endDate: ''
+    }
+  ]);  
+
+  client.close();
+  process.exit(0);
+
+})();
+ 
