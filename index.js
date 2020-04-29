11652027e8e9912c19c29dec25cbbcb0c36a9b4e
Artem Shymchuk <artem.shymchuk@eleks.com>
2020-04-29 08:45:50 +0300

    add price basket logic

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const offerService = require('./offer-service');
+
+async function priceBasket({db, date, basket}) {
+  const products = await queryProducts({db, basket});
+  const activeOffers = await queryActiveOffers({db, date});
+
+  const offers = activeOffers.filter(offer => offerService.isFullfilled({offer, products}));
+
+  const subTotal = calculateSubTotal(products);
+  const discount = offerService.calculateDiscount({offers, products});
+  const discountDescription = offerService.generateDescription({offers, products}) || '(no offers available)';
+  let total = subTotal;
+  if (discount) {
+    total = total - discount;
+  }
+
+  return {subTotal, discountDescription, total};
+}
+
+async function queryProducts({db, basket}) {
+  let products = await db.collection('products').find({
+    name: {'$in': basket.map(item => item.name)}
+  }).toArray();
+
+  products = products.map(product => ({
+    ...product,
+    amount: basket.find(item => item.name === product.name).amount,
+  }));
+
+  return products;
+}
+
+async function queryActiveOffers({db, date}) {
+  return db.collection('offers').find({}).toArray();
+}
+
+function calculateSubTotal(products) {
+  return products.reduce((sum, product) => {
+    sum += product.price * product.amount;
+    return sum;
+  }, 0);
+}
+
+module.exports = {
+  priceBasket
+};
---
+++ offer-service.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const generateDescription = (name, discountSize, discount) => `${name} ${discountSize*100}% off: -$${discount}`;
+
+const findById = (products, id) => products.find(product => product.id === id);
+
+const offerTypes = {
+  discount: {
+      isFullfilled:  ({offer, products}) => {
+        return products.some(product => product.id === offer.details.productId)
+      },
+      calculateDiscount: ({offer, products}) => {
+          const product = findById(products, offer.details.productId);
+          return product.amount * product.price * discountSize;
+      },
+      generateDescription: ({offer, products}) => {
+        const product = findById(products, offer.details.productId);
+        const discount = product.amount * product.price * discountSize;
+        return generateDescription(product.name, offer.details.discountSize, discount);
+      },
+  },
+  package: {
+      isFullfilled:  ({offer, products}) => {
+        return offer.details.every(offerDetail => {
+          const product = findById(products, offerDetail.productId);
+
+          if (!product) {
+            return false;
+          }
+
+          const requiredAmount = offerDetail.requiredAmount || 1;
+          return product.amount >= requiredAmount;
+        })
+      },
+      calculateDiscount: ({offer, products}) => {
+        const detailsWithDiscount = offer.details.filter(detail => detail.discountSize);
+        return detailsWithDiscount.reduce((sum, detail) => {                
+          const product = findById(products, detail.productId);
+          sum += product.price * detail.discountSize;
+          return sum;
+        }, 0)   
+      },
+      generateDescription: ({offer, products}) => {
+        const detailsWithDiscount = offer.details.filter(detail => detail.discountSize);
+        return detailsWithDiscount.map(detail => {
+          const product = findById(products, detail.productId);
+          const discount = product.price * detail.discountSize;
+          return generateDescription(product.name, detail.discountSize, discount);
+        })
+      },
+  }
+};
+
+const offerService = {
+  isFullfilled: ({offer, products}) => offerTypes[offer.type].isFullfilled({offer, products}),
+  generateDescription: ({offers, products}) => {
+    return offers.map(offer => {
+      return offerTypes[offer.type].generateDescription({offer, products})
+    }).flat().join('\n');
+  },
+  calculateDiscount: ({offers, products}) => {
+    return offers.reduce((sum, offer) => {
+      sum += offerTypes[offer.type].calculateDiscount({offer, products});
+      return sum;
+    }, 0);
+  }
+};
+
+module.exports = offerService;
